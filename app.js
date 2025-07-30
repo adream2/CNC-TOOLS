909827819ecf07dfe3e22d2d8d534a91cc341d29
FloatingDust <adream2@qq.com>
2025-07-30 20:54:15 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+// app.js
+App({
+  onLaunch() {
+    // 应用初始化时执行
+    
+  },
+  onShow(options) {
+    // 应用显示时执行
+  },
+  onHide() {
+    // 应用隐藏时执行
+  },
+  globalData: {
+    currentPostId: null
+  }
+})
