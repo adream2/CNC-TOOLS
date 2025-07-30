e26ae384810d64d326740b2025599ae24e37fad6
FloatingDust <adream2@qq.com>
2025-07-30 21:06:44 +0800

    config.js

--- pages/article/config.js
+++ pages/article/config.js
@@ -1,6 +1,6 @@
 const config = {
   // WordPress网站的API域名
-  apiBaseUrl: 'https://lxzt.fun/wp-json/wp-mini-program/v1'
+  apiBaseUrl: 'https://your website/wp-json/wp-mini-program/v1'
 }
 
 module.exports = config
